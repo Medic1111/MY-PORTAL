443b2dec99c458c0ed262125bffc25036f0056c4
Aryse Tansy <medictansy@gmail.com>
2022-06-07 15:25:43 -0400

    Restored original structure of server

--- server/index.js
+++ server/index.js
@@ -7,11 +7,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
 
-// // DATABASE
-// mongoose.connect(process.env.DB_URI, (err) =>
-//   err ? console.log(err) : console.log("Connected to DB")
-// );
-
 // GENERAL MIDDLEWARES
 app.use(
   cors({
@@ -23,20 +18,75 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // PERSONAL REQUIRES
-const registerRoute = require("./routes/register");
-const loginRoute = require("./routes/login");
-const logoutRoute = require("./routes/logout");
+// const registerRoute = require("./routes/register");
+// const loginRoute = require("./routes/login");
+// const logoutRoute = require("./routes/logout");
 
 // PERSONAL MIDDLEWARES
 
-app.use("/", registerRoute);
-app.use("/", loginRoute);
-app.use("/", logoutRoute);
+// app.use("/", registerRoute);
+// app.use("/", loginRoute);
+// app.use("/", logoutRoute);
 
 // DATABASE
 mongoose.connect(process.env.DB_URI, (err) =>
   err ? console.log(err) : console.log("Connected to DB")
 );
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  studentId: String,
+  classes: Array,
+  password: String,
+  email: String,
+});
+
+const Student = new mongoose.model("Student", studentSchema);
+
+// LOGIN
+
+app.post("/api/login", (req, res) => {
+  const { studentId, password } = req.body;
+  Student.find({ studentId, password }, (err, student) =>
+    err ? console.log(err) : res.send(student)
+  );
+});
+
+// REGISTER
+
+app.post("/api/register", (req, res) => {
+  const { name, password, email, studentId, classes } = req.body;
+  let newStudent = new Student({
+    name,
+    password,
+    email,
+    studentId,
+    classes,
+  });
+  newStudent.save((err, student) =>
+    err ? console.log(err) : res.send(student)
+  );
+});
+
+// LOGOUT-UPDATE
+
+app.put("/api/:id/logout", (req, res) => {
+  const { id } = req.params;
+  const { name, password, email, studentId, classes } = req.body;
+  Student.findOneAndUpdate(
+    { _id: id },
+    {
+      name,
+      password,
+      email,
+      studentId,
+      classes,
+    },
+    { new: true, returnOriginal: false },
+    (err, success) =>
+      err ? req.status(400).send(err) : res.status(204).send(success)
+  );
+});
 
 // UNHANDLED
 app.get("*", (req, res) => {
